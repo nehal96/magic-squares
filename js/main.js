a5f683344fc193b36b337bdc3d4ad2dc76452fd1
Nehal Udyavar <nehaludyavar@gmail.com>
2018-04-19 15:34:55 -0700

    Emptied cells and wrote method to make unique ID for each grid cell

--- js/main.js
+++ js/main.js
@@ -3,21 +3,35 @@ var intro_table = d3.select('#intro-magic-square')
               .append('table')
               .append('tbody');
 
-drawSquareGrid(intro_table, 3);
+drawSquareGrid(intro_table, 'intro', 3);
 
 
-function drawSquareGrid(table_element, n) {
+// The function takes 3 variables:
+//    table_element: the table element that the grid will go into (might change
+//        this to parent element and append table inside function?).
+//    grid_short_name: A short name for the specific grid so that each cell can
+//        be given a unique ID.
+//    n: The number of rows or columns you want in the grid (i.e. it will
+//        draw a nxn grid).
+// Returns:
+//    Draws an n x n grid.
+function drawSquareGrid(table_element, grid_short_name, n) {
   var table = table_element,
-      count = 1;
+      grid_name_text = grid_short_name + '-'
+      row_num = 1;
 
   for (var i = 0; i < n; i++) {
-    var row = table.append('tr')
+    var row = table.append('tr'),
+        col_num = 1;
 
     for (var j = 0; j < n; j++) {
       row.append('td')
-         .text(count)
+         .attr('id', grid_name_text + 'r' + row_num + 'c' + col_num)
+         .text('')
 
-      count++;
+      col_num++;
     }
+
+    row_num++;
   }
 }
